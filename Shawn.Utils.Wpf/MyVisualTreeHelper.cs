cc27a3b4a457c612805e323a4b3b6f22a41b9924
Shawn <veckshawn@gmail.com>
2022-12-22 00:07:59 +0800

    + FindVisualChilds

--- Shawn.Utils.Wpf/MyVisualTreeHelper.cs
+++ Shawn.Utils.Wpf/MyVisualTreeHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -100,6 +101,39 @@ namespace Shawn.Utils.Wpf
             return null;
         }
 
+
+
+        public static List<T> FindVisualChilds<T>(DependencyObject obj) where T : DependencyObject
+        {
+            /******
+            // 按住鼠标滚动滚轮时，显示选取框
+            var onScrollChange = (RoutedEventHandler)delegate (object sender, RoutedEventArgs args)
+            {
+                ItemsPresenter ip = VisualTreeHelper.FindVisualChild<ItemsPresenter>(sender as ListView);
+                ScrollContentPresenter p = VisualTreeHelper.VisualUpwardSearch<ScrollContentPresenter>(ip);
+                if (GetIsDragging(p) && Mouse.LeftButton == MouseButtonState.Pressed)
+                {
+                    UpdatePosition(p, true);
+                }
+            };
+              ******/
+
+            var list = new List<T>();
+            // Search immediate children first (breadth-first)
+            for (int i = 0; i < System.Windows.Media.VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = System.Windows.Media.VisualTreeHelper.GetChild(obj, i);
+                if (child is T o)
+                {
+                    list.Add(o);
+                }
+                else
+                {
+                    list.AddRange(FindVisualChilds<T>(child));
+                }
+            }
+            return list;
+        }
         #endregion Static Func
     }
 }
